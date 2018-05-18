6869c5b493759e53bbdfcd8903be0636a4bf87dc
Irakli Gozalishvili <rfobic@gmail.com>
2018-05-17 17:51:25 -0700

    Save work-in-progress

--- demo/background.js
+++ demo/background.js
@@ -6,8 +6,8 @@ browser.protocol.registerProtocol("goz", request => {
     contentType: "text/html",
     content: async function*() {
       const encoder = new TextEncoder("utf-8")
-      yield encoder.encode("hello")
-      yield encoder.encode("world")
+      yield encoder.encode("hello").buffer
+      yield encoder.encode("world").buffer
     }
   }
 })
