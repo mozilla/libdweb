305fd4bf59c967d203c033d94459c36896f44631
Irakli Gozalishvili <rfobic@gmail.com>
2018-06-19 22:18:15 -0700

    write some interface defs

--- src/TCPSocket/TCPSocket.js
+++ src/TCPSocket/TCPSocket.js
@@ -1,54 +1,48 @@
-// @flow
+// @flow strict
 
-export interface UDPSocketManager {
-  create(SocketOptions): Promise<UDPSocket>;
-  close(UDPSocket): Promise<void>;
-  send(
-    UDPSocket,
-    host: string,
-    port: number,
-    data: ArrayBuffer,
-    size?: number
-  ): Promise<number>;
-  messages(UDPSocket): AsyncIterator<UDPMessage>;
-  setMulticastLoopback(UDPSocket, boolean): Promise<void>;
-  setMulticastInterface(UDPSocket, string): Promise<void>;
-  addMembership(
-    UDPSocket,
-    address: string,
-    multicastInterface?: string
-  ): Promise<void>;
-  dropMembership(
-    UDPSocket,
-    address: string,
-    multicastInterface?: string
-  ): Promise<void>;
+interface Client {
+  +id: string;
 }
 
-export type FAMILY_INET = 1
-export type FAMILY_INET6 = 2
-export type FAMILY_LOCAL = 3
-export type Family = FAMILY_INET | FAMILY_INET6 | FAMILY_LOCAL
-
-export interface SocketAddress {
-  host: string;
-  port: number;
-  family: Family;
+interface Server {
+  +id: string;
 }
 
-export interface UDPSocket {
-  id: string;
-  address: SocketAddress;
+interface TCPServerSocket extends Server, AsyncIterator<TCPClientSocket> {
+  +localPort: number;
 }
 
-export interface SocketOptions {
-  host?: string;
-  port?: number;
-  loopbackOnly?: boolean;
-  addressReuse?: boolean;
+type Status = "connecting" | "open" | "closing" | "closed"
+
+interface TCPClientSocket extends Client, AsyncIterator<ArrayBuffer> {
+  +host: string;
+  +port: number;
+  +ssl: boolean;
 }
 
-export interface UDPMessage {
-  from: SocketAddress;
-  data: ArrayBuffer;
+interface ClientManager {
+  connect({ port: number, host?: string }): Promise<TCPClientSocket>;
+  close(Client): void;
+  closeImmediately(Client): void;
+  suspend(Client): void;
+  resume(Client): void;
+  getBufferedAmount(Client): number;
+  getStatus(Client): Status;
+  write(
+    Client,
+    ArrayBuffer,
+    options: ?{ byteOffset?: number, byteLength?: number }
+  ): Promise<boolean>;
+  read(Client): Promise<ArrayBuffer>;
+
+  drained(Client): Promise<void>;
+  errored(Client): Promise<Error>;
+  opened(Client): Promise<void>;
+}
+
+interface ServerManager {
+  server({ port: number, backlog?: number }): TCPServerSocket;
+
+  onconnect(Server): Promise<TCPClientSocket>;
+  close(Server): void;
 }
