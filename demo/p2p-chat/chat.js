70560ed948748453b8593a5c441c55dae9bb51a4
andre alves garzia <andre@andregarzia.com>
2018-07-28 04:41:24 +0200

    adding p2p chat

---
+++ demo/p2p-chat/chat.js
@@ -0,0 +1,82 @@
+var encoder = new TextEncoder()
+var decoder = new TextDecoder()
+var chatMessages = document.querySelector(".chat-messages")
+var textToSend = document.querySelector("[name=msg]")
+var nicknameElement = document.querySelector("[name=nickname]")
+var sendChatButton = document.querySelector("#send")
+var multicastGroup = "239.1.2.3"
+
+var appendChatMessage = payload => {
+  var el = document.createDocumentFragment()
+  var p = document.createElement("p")
+  var data = JSON.parse(payload)
+  p.innerText = `${data.nickname}: ${data.content}`
+  p.style.color = colorForName(data.nickname)
+  console.log("payload", data)
+  console.log("color", colorForName(data.nickname))
+  el.appendChild(p)
+  chatMessages.appendChild(el)
+  chatMessages.scrollTop = chatMessages.scrollHeight
+}
+
+var colorForName = name => {
+  var colours = ['#E37B40', '#46B29D', '#DE5B49', '#324D5C', 'F0C94D'];
+  var sum = 0;
+  for (var i = 0; i < name.length; i++) {
+    sum += name.charCodeAt(i);
+  }
+  return colours[sum % colours.length];
+}
+
+
+var sendChat = async ev => {
+  var content = textToSend.value || "no content"
+  var nickname = nicknameElement.value || "nameless being"
+  var kind = "chat"
+  // builing a flexible payload below so that I can enhance this demo
+  // with messages beyond chat later...
+  var payload = JSON.stringify({
+    kind,
+    nickname,
+    content
+  })
+  const client = await browser.UDPSocket.create({
+    host: "0.0.0.0",
+    port: 41235
+  })
+  textToSend.value = ""
+  const encoder = new TextEncoder()
+
+  // multicasting
+  await browser.UDPSocket.addMembership(client, multicastGroup)
+  const multi_message = encoder.encode(payload).buffer
+  await browser.UDPSocket.send(client, multicastGroup, 41235, multi_message)
+}
+
+var serve = async () => {
+  const server = await browser.UDPSocket.create({
+    host: '0.0.0.0',
+    port: 41235
+  })
+  console.log("adding to multicast group:", multicastGroup)
+  browser.UDPSocket.setMulticastInterface(server, multicastGroup)
+  browser.UDPSocket.addMembership(server, multicastGroup)
+  console.log(`udp listening ${server.address.host}:${server.address.port}`)
+
+  const decoder = new TextDecoder()
+  for await (const { from, data } of browser.UDPSocket.messages(server)) {
+    const payload = decoder.decode(data)
+    console.log(`udp server got: ${payload} from ${from.host}:${from.port}`)
+    appendChatMessage(payload)
+  }
+}
+
+sendChatButton.addEventListener("click", sendChat)
+textToSend.addEventListener("keyup", ev => {
+  if (ev.keyCode === 13) {
+    sendChatButton.click()
+  }
+})
+
+serve()
+
