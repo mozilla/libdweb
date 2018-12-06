d3b5f987e927f2c74f4db6365d382471a720c8bc
Andre Alves Garzia <contato@andregarzia.com>
2018-12-06 01:19:40 +0000

    demo: httpd, image serving is broken

--- demo/httpd/server.js
+++ demo/httpd/server.js
@@ -12,83 +12,87 @@ var mimes = {
 }
 
 var createWebServer = async (port, requestHandler) => {
+  async function onconnect(client) {
+    const message = await client.read()
+    const decodedMessage = decoder.decode(message)
+    const marker = decodedMessage.indexOf("\r\n\r\n")
+
+    if (marker !== -1) {
+      const reqHeader = decodedMessage.slice(0, marker).toString()
+      const body = decodedMessage.slice(marker + 4).toString()
+      const reqHeaders = reqHeader.split("\r\n")
+      const reqLine = reqHeaders.shift().split(" ")
+      const headers = reqHeaders.reduce((acc, currentHeader) => {
+        const [key, value] = currentHeader.split(":")
+        return {
+          ...acc,
+          [key.trim().toLowerCase()]: value.trim()
+        }
+      }, {})
+
+      const request = {
+        method: reqLine[0],
+        url: reqLine[1],
+        httpVersion: reqLine[2].split("/")[1],
+        headers,
+        body
+      }
+
+      console.dir("request", request)
+
+      let status = 200,
+        statusText = "OK",
+        headersSent = false
+      const responseHeaders = {
+        server: "Firefox, yeah, really"
+      }
+
+      function setHeader(key, value) {
+        responseHeaders[key.toLowerCase()] = value
+      }
+
+      async function write(chunk) {
+        await client.write(encoder.encode(chunk).buffer)
+      }
+
+      function sendHeaders() {
+        headersSent = true
+        setHeader("date", new Date().toGMTString())
+        write(`HTTP/1.1 ${status} ${statusText}\r\n`)
+        Object.keys(responseHeaders).forEach(headerKey => {
+          write(`${headerKey}: ${responseHeaders[headerKey]}\r\n`)
+        })
+        write("\r\n")
+      }
+
+      function send(body) {
+        if (!headersSent) {
+          if (!responseHeaders["content-length"]) {
+            setHeader("content-length", body ? body.length : 0)
+          }
+          sendHeaders()
+        }
+        write(body)
+      }
+
+      const response = {
+        setHeader,
+        send,
+        close: async () => await client.close(),
+        setStatus(newStatus, newStatusText) {
+          ;(status = newStatus), (statusText = newStatusText)
+        }
+      }
+
+      await requestHandler(request, response)
+    }
+  }
+
   async function listen() {
     const server = await browser.TCPSocket.listen({ port: port })
 
     for await (const client of server.connections) {
-      const message = await client.read()
-      const decodedMessage = decoder.decode(message)
-      const marker = decodedMessage.indexOf("\r\n\r\n")
-
-      if (marker !== -1) {
-        const reqHeader = decodedMessage.slice(0, marker).toString()
-        const body = decodedMessage.slice(marker + 4).toString()
-        const reqHeaders = reqHeader.split("\r\n")
-        const reqLine = reqHeaders.shift().split(" ")
-        const headers = reqHeaders.reduce((acc, currentHeader) => {
-          const [key, value] = currentHeader.split(":")
-          return {
-            ...acc,
-            [key.trim().toLowerCase()]: value.trim()
-          }
-        }, {})
-
-        const request = {
-          method: reqLine[0],
-          url: reqLine[1],
-          httpVersion: reqLine[2].split("/")[1],
-          headers,
-          body
-        }
-
-        console.dir("request", request)
-
-        let status = 200,
-          statusText = "OK",
-          headersSent = false
-        const responseHeaders = {
-          server: "Firefox, yeah, really"
-        }
-
-        function setHeader(key, value) {
-          responseHeaders[key.toLowerCase()] = value
-        }
-
-        async function write(chunk) {
-          await client.write(encoder.encode(chunk).buffer)
-        }
-
-        function sendHeaders() {
-          headersSent = true
-          setHeader("date", new Date().toGMTString())
-          write(`HTTP/1.1 ${status} ${statusText}\r\n`)
-          Object.keys(responseHeaders).forEach(headerKey => {
-            write(`${headerKey}: ${responseHeaders[headerKey]}\r\n`)
-          })
-          write("\r\n")
-        }
-
-        function send(body) {
-          if (!headersSent) {
-            if (!responseHeaders["content-length"]) {
-              setHeader("content-length", body ? body.length : 0)
-            }
-            sendHeaders()
-          }
-          write(body)
-        }
-
-        const response = {
-          setHeader,
-          send,
-          close: async () => await client.close(),
-          setStatus(newStatus, newStatusText) {
-            ;(status = newStatus), (statusText = newStatusText)
-          }
-        }
-
-        await requestHandler(request, response)
-      }
+      onconnect(client)
     }
   }
   await listen(port)
