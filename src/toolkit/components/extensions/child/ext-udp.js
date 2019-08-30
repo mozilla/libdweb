c6b17e9c1272109795f1ca0e2870ebae74b922dd
Sam Macbeth <sam@cliqz.com>
2019-08-30 18:21:41 +0200

    Parent UDP implementation - passing tests

--- src/toolkit/components/extensions/child/ext-udp.js
+++ src/toolkit/components/extensions/child/ext-udp.js
@@ -20,28 +20,7 @@ interface Host {
 Cu.importGlobalProperties(["URL"])
 
 {
-  const { Services } = Cu.import("resource://gre/modules/Services.jsm", {})
-  const { OS } = Cu.import("resource://gre/modules/osfile.jsm", {})
-  const { ExtensionUtils } = Cu.import(
-    "resource://gre/modules/ExtensionUtils.jsm",
-    {}
-  )
-
-  const { ExtensionError } = ExtensionUtils
-
-  const $Symbol /*:any*/ = Symbol
-
-  class IOError extends ExtensionError {
-    static throw(message) /*:empty*/ {
-      const self = new this(message)
-      throw self
-    }
-  }
-
-  const wrapUnprivilegedFunction = (f, scope) => input =>
-    f(Cu.cloneInto(input, scope))
-
-  const getAPIClasses = (context, refs, sockets) => {
+  const getAPIClasses = context => {
     class UDPSocketClient /*::implements UDPSocket*/ {
       /*::
       address:SocketAddress
@@ -50,14 +29,17 @@ Cu.importGlobalProperties(["URL"])
         throw TypeError("Illegal constructor")
       }
       close() {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          sockets.delete(socket)
-          socket.close()
-          return voidPromise
-        } else {
-          throw notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            await context.childManager.callParentAsyncFunction(
+              "UDPSocket.close",
+              [this.__id]
+            )
+            resolve()
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
       send(
         host /*: string*/,
@@ -65,74 +47,83 @@ Cu.importGlobalProperties(["URL"])
         data /*: ArrayBuffer*/,
         size /*::?: number*/
       ) /*: Promise<number>*/ {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          const n = socket.send(
-            host,
-            port,
-            new Uint8Array(data),
-            size || data.byteLength
-          )
-          return context.cloneScope.Promise.resolve(n)
-        } else {
-          return notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            const result = await context.childManager.callParentAsyncFunction(
+              "UDPSocket.send",
+              [this.__id, host, port, data, size]
+            )
+            resolve(result)
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
       messages() {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          const host = new MessagesHost(socket)
-          const client = exportInstance(context.cloneScope, Messages)
-          refs.messages.set(client, host)
-          socket.asyncListen(host)
-          return client
-        } else {
-          return notFoundPromise()
-        }
+        const client = exportInstance(context.cloneScope, Messages, {
+          __socketId: this.__id
+        })
+        return client
       }
       setMulticastLoopback(flag /*:boolean*/) /*: Promise<void>*/ {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          socket.multicastLoopback = flag
-          return voidPromise
-        } else {
-          return notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            await context.childManager.callParentAsyncFunction(
+              "UDPSocket.setMulticastLoopback",
+              [this.__id, flag]
+            )
+            resolve()
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
       setMulticastInterface(
         multicastInterface /*:string*/
       ) /*: Promise<void>*/ {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          socket.multicastInterface = multicastInterface
-          return voidPromise
-        } else {
-          return notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            await context.childManager.callParentAsyncFunction(
+              "UDPSocket.setMulticastInterface",
+              [this.__id, multicastInterface]
+            )
+            resolve()
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
       joinMulticast(
         address /*: string*/,
         multicastInterface /*::?: string*/
       ) /*: Promise<void>*/ {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          socket.joinMulticast(address, multicastInterface)
-          return voidPromise
-        } else {
-          return notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            await context.childManager.callParentAsyncFunction(
+              "UDPSocket.joinMulticast",
+              [this.__id, address, multicastInterface]
+            )
+            resolve()
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
       leaveMulticast(
         address /*: string*/,
         multicastInterface /*::?: string*/
       ) /*: Promise<void>*/ {
-        const socket = refs.sockets.get(this)
-        if (socket) {
-          socket.leaveMulticast(address, multicastInterface)
-          return voidPromise
-        } else {
-          return notFoundPromise()
-        }
+        return new context.cloneScope.Promise(async (resolve, reject) => {
+          try {
+            await context.childManager.callParentAsyncFunction(
+              "UDPSocket.leaveMulticast",
+              [this.__id, address, multicastInterface]
+            )
+            resolve()
+          } catch (e) {
+            reject(e.message)
+          }
+        })
       }
     }
 
@@ -144,132 +135,16 @@ Cu.importGlobalProperties(["URL"])
         throw TypeError("Illegal constructor")
       }
       next() {
-        const host = refs.messages.get(this)
-        if (host) {
-          return host.next()
-        } else {
-          return notFoundPromise()
-        }
-      }
-      return() {
-        const host = refs.messages.get(this)
-        if (host) {
-          return host.return()
-        } else {
-          return notFoundPromise()
-        }
-      }
-    }
-
-    class MessagesHost {
-      /*::
-      socket:nsIUDPSocket
-      requests:{resolve({done:false, value:UDPMessage}|{done:true}):void, reject(Error):void}[]
-      responses:Promise<{done:false, value:UDPMessage}>[]
-      isDone:boolean
-      done:Promise<{done:true, value:void}>
-      scope:Object
-      */
-      constructor(socket) {
-        this.socket = socket
-        this.isDone = false
-        this.requests = []
-        this.responses = []
-        this.scope = context.cloneScope
-      }
-      onPacketReceived(socket, message) {
-        const { scope } = this
-        debug && console.log(`TCPSocket/onPacketReceived`, message)
-        const { address, port, family } = message.fromAddr
-        this.continue(
-          Cu.cloneInto(
-            [
-              message.rawData.buffer,
-              Cu.cloneInto({ address, port, family }, scope)
-            ],
-            scope
-          )
+        return context.childManager.callParentAsyncFunction(
+          "UDPSocket.pollMessages",
+          [this.__socketId]
         )
       }
-      onStopListening(socket, status) {
-        debug && console.log(`TCPSocket/onStopListening`, status)
-
-        if (status === Cr.NS_BINDING_ABORTED) {
-          return this.break()
-        } else {
-          return this.throw(
-            new Error(`Socket was closed with error: ${status}`)
-          )
-        }
-      }
-      continue(value) {
-        const { requests, isDone, responses, scope } = this
-        if (isDone) {
-          throw Error("Received message after iteration was done")
-        } else {
-          debug && console.log("UDPSocket.MessagesHost.continue", value)
-          const nextIteration = Cu.cloneInto(
-            { done: false /*::,value:value*/ },
-            context.cloneScope
-          )
-          Reflect.defineProperty(Cu.unwaiveXrays(nextIteration), "value", {
-            value
-          })
-
-          const request = requests.shift()
-          if (request) {
-            request.resolve(nextIteration)
-          } else {
-            responses.push(scope.Promise.resolve(nextIteration))
-          }
-        }
-      }
-      break() {
-        const { requests } = this
-        for (const request of requests) {
-          request.resolve(doneIteration)
-        }
-      }
-      throw(error) {
-        const { requests } = this
-        for (const request of requests) {
-          request.reject(error)
-        }
-      }
-      next() {
-        const { responses, requests, done, isDone } = this
-        const response = responses.shift()
-        if (response) {
-          return response
-        } else if (isDone) {
-          return done
-        } else {
-          return new context.cloneScope.Promise((resolve, reject) => {
-            requests.push({ resolve, reject })
-          })
-        }
-      }
       return() {
-        const { isDone, done } = this
-        if (!isDone) {
-          this.isDone = true
-          this.socket.close()
-        }
-        return doneIteration
-      }
-    }
-
-    const voidPromise /*:Promise<void>*/ = context.cloneScope.Promise.resolve()
-    const doneIteration = Cu.cloneInto({ done: true }, context.cloneScope)
-    const notFound = new ExtensionError("Host for the object not found")
-    let notFoundPromiseCache = null
-
-    const notFoundPromise = () => {
-      if (notFoundPromiseCache) {
-        return notFoundPromiseCache
-      } else {
-        notFoundPromiseCache = context.cloneScope.Promise.reject(notFound)
-        return notFoundPromiseCache
+        return context.childManager.callParentAsyncFunction(
+          "UDPSocket.returnHost",
+          [this.__socketId]
+        )
       }
     }
 
@@ -290,45 +165,25 @@ Cu.importGlobalProperties(["URL"])
       FAMILY_LOCAL: Ci.nsINetAddr.FAMILY_LOCAL,
 
       create: config =>
-        new context.cloneScope.Promise((resolve, reject) => {
+        new context.cloneScope.Promise(async (resolve, reject) => {
           const options = config || noOptions
+
           try {
-            const socket = Cc[
-              "@mozilla.org/network/udp-socket;1"
-            ].createInstance(Ci.nsIUDPSocket)
-
-            if (options.host != null) {
-              socket.init2(
-                options.host,
-                options.port || -1,
-                null,
-                options.addressReuse != false
-              )
-            } else {
-              socket.init(
-                options.port || -1,
-                options.loopbackOnly != false,
-                null,
-                options.addressReuse != false
-              )
-            }
-
-            const { address, port, family } = socket.localAddr
-            const client /*:UDPSocket*/ = exportInstance(
-              context.cloneScope,
-              api.UDPSocket,
-              {
-                address: Cu.cloneInto(
-                  { address, port, family },
-                  context.cloneScope
-                )
-              }
+            const socket = await context.childManager.callParentAsyncFunction(
+              "UDPSocket.create",
+              [options]
             )
-            sockets.add(socket)
-            refs.sockets.set(client, socket)
+            const client = exportInstance(context.cloneScope, api.UDPSocket, {
+              __id: socket.id,
+              address: {
+                address: socket.address,
+                port: socket.port,
+                family: socket.family
+              }
+            })
             resolve(client)
-          } catch (error) {
-            reject(new ExtensionError(error))
+          } catch (e) {
+            reject(e.message)
           }
         })
     }
@@ -341,15 +196,6 @@ Cu.importGlobalProperties(["URL"])
         messages: new WeakMap()
       }
       const sockets = new Set()
-
-      context.callOnClose({
-        close() {
-          for (const socket of sockets) {
-            socket.close()
-          }
-          sockets.clear()
-        }
-      })
 
       return { UDPSocket: getAPI(context, refs, sockets) }
     }
@@ -417,5 +263,4 @@ Cu.importGlobalProperties(["URL"])
   }
 
   const noOptions = {}
-  const debug = true
 }
---
+++ src/toolkit/components/extensions/parent/ext-udp.js
@@ -0,0 +1,274 @@
+// @flow strict
+
+/*::
+import { Cu, Cr, Ci, Cc, ExtensionAPI } from "gecko"
+
+import type {BaseContext, nsIUDPSocket, nsIUDPSocketListener, nsINetAddr} from "gecko"
+import type {
+  UDPSocket,
+  UDPSocketManager,
+  UDPMessage,
+  SocketAddress,
+  SocketOptions,
+  Family
+} from "../interface/udp"
+
+interface Host {
+  +UDPSocket: UDPSocketManager;
+}
+*/
+Cu.importGlobalProperties(["URL"])
+
+{
+  global.UDPSocket = class extends ExtensionAPI /*::<Host>*/ {
+    getAPI(context) {
+      const sockets = new Map()
+      const messages = new Map()
+      const notFoundPromise = "not found"
+
+      class MessagesHost {
+        /*::
+        socket:nsIUDPSocket
+        requests:{resolve({done:false, value:UDPMessage}|{done:true}):void, reject(Error):void}[]
+        responses:Promise<{done:false, value:UDPMessage}>[]
+        isDone:boolean
+        done:Promise<{done:true, value:void}>
+        scope:Object
+        */
+        constructor(socket) {
+          this.socket = socket
+          this.isDone = false
+          this.requests = []
+          this.responses = []
+          this.scope = context.cloneScope
+        }
+        onPacketReceived(socket, message) {
+          const { scope } = this
+          debug && console.log(`TCPSocket/onPacketReceived`, message)
+          const { address, port, family } = message.fromAddr
+          this.continue(
+            Cu.cloneInto(
+              [
+                message.rawData.buffer,
+                Cu.cloneInto({ address, port, family }, scope)
+              ],
+              scope
+            )
+          )
+        }
+        onStopListening(socket, status) {
+          debug && console.log(`TCPSocket/onStopListening`, status)
+
+          if (status === Cr.NS_BINDING_ABORTED) {
+            return this.break()
+          } else {
+            return this.throw(
+              new Error(`Socket was closed with error: ${status}`)
+            )
+          }
+        }
+        continue(value) {
+          const { requests, isDone, responses, scope } = this
+          if (isDone) {
+            throw Error("Received message after iteration was done")
+          } else {
+            debug && console.log("UDPSocket.MessagesHost.continue", value)
+            const nextIteration = { done: false, value }
+            const request = requests.shift()
+            if (request) {
+              request.resolve(nextIteration)
+            } else {
+              responses.push(scope.Promise.resolve(nextIteration))
+            }
+          }
+        }
+        break() {
+          const { requests } = this
+          for (const request of requests) {
+            request.resolve({ done: true })
+          }
+        }
+        throw(error) {
+          const { requests } = this
+          for (const request of requests) {
+            request.reject(error)
+          }
+        }
+        next() {
+          const { responses, requests, done, isDone } = this
+          const response = responses.shift()
+          if (response) {
+            return response
+          } else if (isDone) {
+            return done
+          } else {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              requests.push({ resolve, reject })
+            })
+          }
+        }
+        return() {
+          const { isDone, done } = this
+          if (!isDone) {
+            this.isDone = true
+            this.socket.close()
+          }
+          return { done: true }
+        }
+      }
+
+      context.callOnClose({
+        close() {
+          for (const socket of sockets.values()) {
+            socket.close()
+          }
+          sockets.clear()
+        }
+      })
+      let socketIdx = 0
+
+      return {
+        UDPSocket: {
+          create: options => {
+            return new Promise((resolve, reject) => {
+              try {
+                const socket = Cc[
+                  "@mozilla.org/network/udp-socket;1"
+                ].createInstance(Ci.nsIUDPSocket)
+
+                if (options.host != null) {
+                  socket.init2(
+                    options.host,
+                    options.port || -1,
+                    null,
+                    options.addressReuse != false
+                  )
+                } else {
+                  socket.init(
+                    options.port || -1,
+                    options.loopbackOnly != false,
+                    null,
+                    options.addressReuse != false
+                  )
+                }
+
+                const { address, port, family } = socket.localAddr
+                const id = ++socketIdx
+                sockets.set(id, socket)
+
+                resolve({
+                  id,
+                  address,
+                  port,
+                  family
+                })
+              } catch (error) {
+                reject(error.message)
+              }
+            })
+          },
+          close: socketId => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                socket.close()
+                sockets.delete(socketId)
+                resolve()
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          send: (socketId, host, port, data, size) => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                const n = socket.send(
+                  host,
+                  port,
+                  new Uint8Array(data),
+                  size || data.byteLength
+                )
+                resolve(n)
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          setMulticastLoopback: (socketId, flag) => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                socket.multicastLoopback = flag
+                resolve()
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          setMulticastInterface: (socketId, multicastInterface) => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                socket.multicastInterface = multicastInterface
+                resolve()
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          joinMulticast: (socketId, address, multicastInterface) => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                socket.joinMulticast(address, multicastInterface)
+                resolve()
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          leaveMulticast: (socketId, address, multicastInterface) => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (sockets.has(socketId)) {
+                const socket = sockets.get(socketId)
+                socket.leaveMulticast(address, multicastInterface)
+                resolve()
+              } else {
+                reject(notFoundPromise)
+              }
+            })
+          },
+          pollMessages: socketId => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (!sockets.has(socketId)) {
+                return reject(notFoundPromise)
+              }
+              let host
+              if (!messages.has(socketId)) {
+                const socket = sockets.get(socketId)
+                host = new MessagesHost(socket)
+                socket.asyncListen(host)
+                messages.set(socketId, host)
+              } else {
+                host = messages.get(socketId)
+              }
+              resolve(host.next())
+            })
+          },
+          returnHost: socketId => {
+            return new context.cloneScope.Promise((resolve, reject) => {
+              if (!sockets.has(socketId) || !messages.has(socketId)) {
+                return reject(notFoundPromise)
+              }
+              const result = messages.get(socketId).return()
+              resolve(result)
+            })
+          }
+        }
+      }
+    }
+  }
+
+  const debug = false
+}
